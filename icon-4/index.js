5534e8fea5312eb382677c56f735368d5d996b02
Max <pots1988bear@gmail.com>
2024-04-10 11:37:49 +0200

    Added button #4 effect

---
+++ icon-4/index.js
@@ -0,0 +1,13 @@
+const buttons = document.querySelectorAll('.btn');
+const ACTIVE_CLASS = 'activated';
+const AUTO_REMOVE_CLASS_DELAY = 2000;
+
+buttons.forEach((btn) => btn.addEventListener('click', (e) => {
+  const btnHasActiveClass = btn.classList.contains(ACTIVE_CLASS);
+
+  btn.classList.toggle(ACTIVE_CLASS);
+
+  if (!btnHasActiveClass) {
+    setTimeout(() => btn.classList.remove(ACTIVE_CLASS), AUTO_REMOVE_CLASS_DELAY)
+  }
+}));
